7beb661c8bdf7bdfdd4351c2b8c1eea772715777
Artyom Andreev <andreev.artiom@gmail.com>
2021-08-21 00:51:15 +0300

    init: start task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    return array.reduce((x, y) => x < y ? x : y);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    return array.reduce((x, y) => x > y ? x : y);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    return array.reduce((x, y) => x + y) / array.length;
 }
